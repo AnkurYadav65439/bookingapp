c33abc71a2c24be0f52ac3c48d30cdd923a8ec0b
AnkurYadav65439 <ay0695617@gmail.com>
2023-11-29 13:59:38 +0530

    creating errorHandler and middleware

--- api/index.js
+++ api/index.js
@@ -8,18 +8,36 @@ import roomsRoute from './routes/rooms.js'
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO);
+mongoose.connect(process.env.MONGO)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.listen(port, ()=>{
-    console.log('Server listening on port' , port);
+app.listen(port, () => {
+    console.log('Server listening on port', port);
 })
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", usersRoute);
 app.use("/api/hotels", hotelsRoute);
 app.use("/api/rooms", roomsRoute);
+
+//middleware for error 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode,
+        stack: err.stack
+    });
+})
