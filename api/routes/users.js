5889490c917801a5e5637470b60236460d91a0e8
AnkurYadav65439 <ay0695617@gmail.com>
2023-12-01 18:44:28 +0530

    complete users and hotels, auth routes controllers

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import { deleteUser, getUser, getUsers, updateUser } from '../controllers/userController.js';
+import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
+router.put("/:id", verifyUser, updateUser);
+router.delete("/:id", verifyUser, deleteUser);
+router.get("/:id", verifyUser, getUser);
+router.get("/", verifyAdmin, getUsers);
 
 export default router
