7a8c94397c792afe88ed6cc07e47e68c457673a6
AnkurYadav65439 <ay0695617@gmail.com>
2023-12-02 20:34:38 +0530

    room model and its routes controllers.

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from '../controllers/roomController.js';
+import { verifyAdmin } from '../utils/verifyToken.js';
 
 const router = express.Router();
 
+router.post("/:hid", verifyAdmin, createRoom);
+router.put("/:id", verifyAdmin, updateRoom);
+router.delete("/:id/:hid", verifyAdmin, deleteRoom);
+router.get("/:id", getRoom);
+router.get("/", getRooms);
 
 export default router
